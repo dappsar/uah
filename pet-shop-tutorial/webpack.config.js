613c13ee707cbd2ecbf058ca02fa769fb8bbc4c9
Diego Baranowski <diego.baranowski@enerminds.com>
2018-10-22 07:11:57 -0300

    changed webpack

--- pet-shop-tutorial/webpack.config.js
+++ pet-shop-tutorial/webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path')
+const json1 = require('build/contracts/Adoption.json');
+const json2 = require('build/contracts/Migration.json');
 
 module.exports = {
 	entry: path.join(__dirname, 'src/js', 'app.js'), 
 	output: {
-	filename: 'build.js',
-	path: path.join(__dirname, 'dist')
-},
-module: {
-   rules: [{
-	  test: /\.css$/, // To load the css in react
-	  use: ['style-loader', 'css-loader'],
-	  include: [
-		path.resolve(__dirname, "src/"),
-		path.resolve(__dirname, "build/contracts/")
-	  ]
-   }]
-}
+		filename: 'build.js',
+		path: path.join(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader'],
+			include: [
+				path.resolve(__dirname, "src/"),
+				path.resolve(__dirname, "build/contracts/")
+			]
+		}],
+		loaders: [
+			{
+			test: /\.json$/,
+			loader: 'json-loader'
+			}
+		]   
+	}
 }
