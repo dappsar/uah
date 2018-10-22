ca8e73feefab61001cb44083af98d502f85d34d9
Diego Baranowski <diego.baranowski@enerminds.com>
2018-10-22 13:27:32 -0300

    changed truffle config

--- pet-shop-tutorial/truffle.js
+++ pet-shop-tutorial/truffle.js
@@ -1,3 +1,4 @@
+/*
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,5 +8,17 @@ module.exports = {
       network_id: "4",
       from: "0x94a9c4f8eb4e40e394d9800b69c42a18fee2af7b",
       gas: 6712390
+  }
+};
+*/
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
   }
 };
