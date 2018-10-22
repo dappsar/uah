84bcc2bf2fc75d89e4d9f3f2ab395bc6c4dd7c33
Diego Baranowski <diego.baranowski@enerminds.com>
2018-10-22 13:23:13 -0300

    changed truffle script to set rinkeby network

--- pet-shop-tutorial/truffle.js
+++ pet-shop-tutorial/truffle.js
@@ -1,11 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
+  rinkeby: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
+      port: 8545,
+      network_id: "4",
+      from: "0x94a9c4f8eb4e40e394d9800b69c42a18fee2af7b",
+      gas: 6712390
   }
 };
